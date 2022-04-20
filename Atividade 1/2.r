b52f0f1e84f5dc487d0765a1dd95a438f71b5495
João Victor Araujo <jvsa@ic.ufal.br>
2022-04-20 18:23:25 -0300

    Questão 2 finalizada

--- Atividade 1/2.r
+++ Atividade 1/2.r
@@ -1,13 +1,22 @@
 library(readxl)
 library(tcltk)
-library(dplyr)
-
-
 
 dados <- read_excel("ENEM_AL_EXCEL_AJUS_OKSNZ.xlsx")
 municipio <- subset(dados, NO_MUNICIPIO_PROVA == "Rio Largo")
-notas <- data.frame(municipio$NU_NOTA_REDACAO, municipio$NU_NOTA_CN, municipio$NU_NOTA_CH,
-                    municipio$NU_NOTA_LC, municipio$NU_NOTA_MT, municipio$NOTA_ENEN)
-notas
+alunos <- subset(municipio, TP_SEXO == "Masculino")
+alunas <- subset(municipio, TP_SEXO == "Feminino")
+notas_alunos <- alunos$NOTA_ENEN
+notas_alunas <- alunas$NOTA_ENEN
+windows()
+hist(notas_alunos, breaks = 10, main = "2 - Histograma das notas dos alunos",
+                    col = "blue", xlab = "Notas", ylab = "Frequencias")
 
-summarise(group_by())
+prompt  <- "Aperte a barra de espaco para fechar o grafico"
+capture <- tk_messageBox(message = prompt)
+
+windows()                    
+hist_alunas <- hist(notas_alunas, breaks = 10, main = "2 - Histograma das notas dos alunas",
+                    col = "red", xlab = "Notas", ylab = "Frequencias")
+
+prompt  <- "Aperte a barra de espaco para fechar o grafico"
+capture <- tk_messageBox(message = prompt)
